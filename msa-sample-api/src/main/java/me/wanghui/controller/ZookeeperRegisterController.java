8e84efcfccddfffdc91d6238bc30944cc837239f
mac <wanghui_2012_vip@163.com>
2017-03-08 14:09:21 +0800

    添加put请求

--- msa-sample-api/src/main/java/me/wanghui/controller/ZookeeperRegisterController.java
+++ msa-sample-api/src/main/java/me/wanghui/controller/ZookeeperRegisterController.java
@@ -2,6 +2,7 @@ package me.wanghui.controller;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.tomcat.util.codec.binary.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,10 @@ import javax.servlet.http.HttpSession;
  */
 
 @RestController
-@RequestMapping(value = "user")
+@RequestMapping(name = "userSayHello", value = "user")
 public class ZookeeperRegisterController {
 
-    @RequestMapping(name = "userSayHello", path = "hello", method = RequestMethod.GET)
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
     public String hello_1(HttpServletRequest request, HttpSession session) {
 
         String name = (String) session.getAttribute("name");
@@ -29,10 +30,10 @@ public class ZookeeperRegisterController {
             System.out.println("session add name : " + name);
         }
         System.out.println("session name : " + name);
-        return "this is api 1";
+        return "this is api user/hello";
     }
 
-    @RequestMapping(name = "userSayHello", path = "hello2", method = RequestMethod.GET)
+    @RequestMapping(path = "hello2", method = RequestMethod.GET)
     public String hello_2(HttpServletRequest request, HttpSession session) {
 
         String name = (String) session.getAttribute("name");
@@ -42,6 +43,17 @@ public class ZookeeperRegisterController {
             System.out.println("session add name : " + name);
         }
         System.out.println("session name : " + name);
-        return "this is api 2";
+        return "this is api user/hello2";
+    }
+
+    @RequestMapping(path = "hello2/{userName}", method = RequestMethod.GET)
+    public String hello_3(HttpServletRequest request, HttpSession session, @PathVariable String userName) {
+
+        return "this is api get request set userName : " + userName;
+    }
+
+    @RequestMapping(path = "hello_put/{userName}", method = RequestMethod.PUT)
+    public String hello_put(HttpServletRequest request, HttpSession session, @PathVariable String userName) {
+        return "this is api put request set userName : " + userName;
     }
 }
